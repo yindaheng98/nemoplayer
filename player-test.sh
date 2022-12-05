29baa32b9d0252bfdc201258c5c8affd31b7835f
yindaheng98 <yindaheng98@gmail.com>
2022-12-05 21:39:27 +0800

    save test result

--- player-test.sh
+++ player-test.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 VIDEOPATH=$1
+DSTPATH=$2
 eval `ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of default=nw=1 $VIDEOPATH`
-ffmpeg -i $VIDEOPATH -v quiet -vcodec rawvideo -f rawvideo -pix_fmt yuv420p pipe:1 | ./build/vpxenc --ivf --passes=1 -w $width -h $height -o text.ivf -
+ffmpeg -i $VIDEOPATH -v quiet -vcodec rawvideo -f rawvideo -pix_fmt yuv420p pipe:1 | \
+./build/vpxenc --ivf --passes=1 -w $width -h $height -o - - | \
+./player > $DSTPATH
