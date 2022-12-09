95f05ba808f2603043b46e8aae953edbaed3c4b5
yindaheng98 <yindaheng98@gmail.com>
2022-12-09 22:19:37 +0800

    ugly code

--- common.h
+++ common.h
@@ -1,5 +1,9 @@
 #include "./video_reader.h"
 
+// VpxVideoReaderStruct is defined in .c
+// So must have a VpxVideoReaderStruct here or you can not make a .o.
+// Fuck the ugly code!
+// One should define a struct in .h rather than .c !!!!!!
 struct VpxVideoReaderStruct {
   VpxVideoInfo info;
   FILE *file;
