f434351dcaf1c4f74a0bbf6063021773f280bf4c
yindaheng98 <yindaheng98@gmail.com>
2022-12-10 16:36:01 +0800

    bug fixed

--- player-test.sh
+++ player-test.sh
@@ -20,6 +20,6 @@ T=$(($FRAMES / $frame_rate))                                                    
 ffmpeg -ss "$FROM" -t $T -i $VIDEOPATH -s ${small_width}x${small_height} -v quiet -vcodec rawvideo -f rawvideo -pix_fmt yuv420p pipe:1 | # 原始视频缩放后转rawvideo
     ./build/vpxenc --ivf --passes=1 -w $small_width -h $small_height -o $SMALLPATH -                                                     # 输入给vpxenc编码为IVF文件
 
-ffmpeg -ss "$FROM" -t $T -i $VIDEOPATH -v quiet -vcodec rawvideo -f rawvideo -pix_fmt yuv420p -vf "select=not(mod(n\,$SKIP))" pipe:1 |                            # 原始视频转rawvideo作为高清低帧率输入
+ffmpeg -ss "$FROM" -t $T -i $VIDEOPATH -v quiet -vcodec rawvideo -f rawvideo -pix_fmt yuv420p -vf "select=not(mod(n\,$SKIP))" -vsync vfr pipe:1 |                 # 原始视频转rawvideo作为高清低帧率输入
     ./player $SMALLPATH - - $SCALE $SKIP |                                                                                                                        # player程序：从文件读低清高帧率视频；从stdin读高清低帧率视频；高清高帧率视频输出到stdout
     ffmpeg -framerate $frame_rate -video_size ${width}x${height} -pixel_format yuv420p -f rawvideo -i pipe:0 -v quiet -c:v libx264 -preset slow -qp 0 -y $DSTPATH # 编码为MP4写入文件，方便看
