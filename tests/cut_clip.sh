75ef29714c9d364e8b938e764bf20214fdd75a92
root <root@localhost.localdomain>
2022-12-16 17:25:01 +0800

    cut and scale down

--- tests/cut_clip.sh
+++ tests/cut_clip.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -x
 
 ORIGPATH=$1 # 原始完整视频路径（读取）
-CLIPPATH=$2 # 裁剪后视频路径（写入）
-START=$3    # 从哪开始剪
-FRAME=$4    # 剪多少帧
+SMALPATH=$2 # 裁剪后视频路径（写入）
+SCALE=$3    # 缩放倍率
+START=$4    # 从哪开始剪
+FRAME=$5    # 剪多少帧
 
-mkdir -p $(dirname $CLIPPATH)
+mkdir -p $(dirname $SMALPATH)
 
-END=$(($START + $FRAME))
-ffmpeg -i $ORIGPATH -vf select="between(n\,$START\,$END),setpts=PTS-STARTPTS" -vsync vfr $CLIPPATH
+eval $(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of default=nw=1 $ORIGPATH) # 获取原始视频尺寸
+small_width=$(($width / $SCALE))                                                                          # 计算缩放后的尺寸
+small_height=$(($height / $SCALE))                                                                        # 计算缩放后的尺寸
+
+END=$(($START + $FRAME - 1))
+ffmpeg -i $ORIGPATH -s ${small_width}x${small_height} -vcodec rawvideo -f rawvideo -pix_fmt yuv420p -vf select="between(n\,$START\,$END),setpts=PTS-STARTPTS" -vsync vfr $SMALPATH -y # 原始视频缩放后转rawvideo
