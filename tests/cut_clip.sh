a80abe29ff7c8fee068f19641976cad14a0c5115
root <root@localhost.localdomain>
2022-12-16 17:28:24 +0800

    simplified

--- tests/cut_clip.sh
+++ tests/cut_clip.sh
@@ -5,12 +5,15 @@ SMALPATH=$2 # 裁剪后视频路径（写入）
 SCALE=$3    # 缩放倍率
 START=$4    # 从哪开始剪
 FRAME=$5    # 剪多少帧
+FFMPEG="ffmpeg -v quiet -y"
+RAWARG="-f rawvideo -pix_fmt yuv420p"
 
 mkdir -p $(dirname $SMALPATH)
 
 eval $(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of default=nw=1 $ORIGPATH) # 获取原始视频尺寸
 small_width=$(($width / $SCALE))                                                                          # 计算缩放后的尺寸
 small_height=$(($height / $SCALE))                                                                        # 计算缩放后的尺寸
+SIZEARG="${small_width}x${small_height}"
 
 END=$(($START + $FRAME - 1))
-ffmpeg -i $ORIGPATH -s ${small_width}x${small_height} -vcodec rawvideo -f rawvideo -pix_fmt yuv420p -vf select="between(n\,$START\,$END),setpts=PTS-STARTPTS" -vsync vfr $SMALPATH -y # 原始视频缩放后转rawvideo
+$FFMPEG -i $ORIGPATH -s $SIZEARG -vcodec rawvideo $RAWARG -vf select="between(n\,$START\,$END),setpts=PTS-STARTPTS" -vsync vfr $SMALPATH # 原始视频缩放后转rawvideo
