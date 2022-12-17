5dce80ca24a073425cd454a0bc9e0ffaec9c64c3
root <root@localhost.localdomain>
2022-12-17 11:32:08 +0800

    data for draw

--- tests/draw.py
+++ tests/draw.py
@@ -4,3 +4,14 @@ import pandas as pd
 path = sys.argv[1]
 data = pd.read_csv(path)
 print(data)
+for video in data["video"].drop_duplicates():
+    video_data = data[data["video"] == video]
+    print(video_data)
+    for i in range(len(video_data)):
+        clip_data = video_data.iloc[i]
+        start = clip_data['start']
+        frame = len(clip_data) - 3
+        clip_x = [start + j for j in range(1, frame+1)]
+        clip_y = [clip_data[str(j)] for j in range(1, frame+1)]
+        print(clip_x)
+        print(clip_y)
