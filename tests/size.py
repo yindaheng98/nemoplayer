8048c8c3316460d8e054826e603d93b16c6cb4af
root <root@localhost.localdomain>
2022-12-24 22:27:24 +0800

    full size

--- tests/size.py
+++ tests/size.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import argparse
 import ffmpeg
@@ -5,9 +6,19 @@ import ffmpeg
 logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--video', type=str, required=True, help='Video to be count')
-parser.add_argument('--frame', type=int, required=True, help='Number of frames')
+parser.add_argument('--name', type=str, required=True, help='Name of the video')
+parser.add_argument('--small', type=str, required=True, help='Small video path')
+parser.add_argument('--datadir', type=str, required=True, help='Dir for data')
+
 
 args = parser.parse_args()
-probe = ffmpeg.probe(args.video, show_frames=None)
-print(",".join([str(frame['pkt_size']) for frame in probe['frames']]))
+logging.info({
+    '--name': args.name,
+    '--small': args.small,
+    '--datadir': args.datadir,
+})
+
+probe = ffmpeg.probe(args.small, show_frames=None)
+path = os.path.join(args.datadir, "size_full.csv")
+with open(path, "a+", encoding='utf8') as f:
+    f.write(f"{args.name},{','.join(str(frame['pkt_size']) for frame in probe['frames'])}\n")
---
+++ tests/size_video.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -x
+
+ORIGIPATH=$1     # 原始完整视频路径（读取）
+SMALLPATH=$2.ivf # 原始高清视频经裁剪缩放后视频路径（写入）
+SCALE=$3         # 缩放倍率
+
+mkdir -p $(dirname $SMALLPATH)
+
+eval $(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of default=nw=1 $ORIGIPATH) # 获取原始视频尺寸
+small_width=$(($width / $SCALE))                                                                           # 计算缩放后的尺寸
+small_height=$(($height / $SCALE))                                                                         # 计算缩放后的尺寸
+SIZEARG="${small_width}x${small_height}"
+
+ENCODER="$(dirname $0)/../build/vpxenc"
+FFMPEG="ffmpeg -v quiet -y"
+RAWARG="-f rawvideo -pix_fmt yuv420p"
+$FFMPEG -i $ORIGIPATH -s $SIZEARG -vcodec rawvideo $RAWARG pipe:1 |            # 原始视频缩放后转rawvideo
+    $ENCODER --ivf --passes=1 -w $small_width -h $small_height -o $SMALLPATH - # 输入给vpxenc编码为IVF文件
+
+DATADIR=$(dirname $0)/data/$DEVICE
+mkdir -p $DATADIR
+QRUN="$(dirname $0)/size.py"
+PYTHONPATH=$(dirname $0) python3 $QRUN --name $(basename $ORIGIPATH) --small $SMALLPATH --datadir $DATADIR
+
+rm $SMALLPATH
