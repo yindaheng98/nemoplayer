bb8d70c594a5c14798d981e67e695a6333589a7b
root <root@localhost.localdomain>
2023-02-02 17:44:06 +0800

    draw more

--- tests/draw_average.py
+++ tests/draw_average.py
@@ -5,15 +5,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 from draw import savedir
 
+savedir = os.path.join(savedir, "summary")
+os.makedirs(savedir, exist_ok=True)
+
+
 def draw(ax, video_data, **kwargs):
-        frame = len(video_data.columns) - 3
-        clip_data = video_data.loc[:, [str(i) for i in range(1, frame+1)]]
-        clip_x = list(range(1, frame+1))
-        clip_data.replace(clip_data.max().max(), np.nan, inplace=True)
-        clip_data.dropna(inplace=True)
-        clip_y = clip_data.mean()
-        print(clip_y)
-        ax.plot(clip_x, clip_y, **kwargs)
+    frame = len(video_data.columns) - 3
+    clip_data = video_data.loc[:, [str(i) for i in range(1, frame+1)]]
+    clip_x = list(range(1, frame+1))
+    clip_data.replace(clip_data.max().max(), np.nan, inplace=True)
+    clip_data.dropna(inplace=True)
+    clip_y = clip_data.mean()
+    print(clip_y)
+    ax.plot(clip_x, clip_y, **kwargs)
+
 
 if __name__ == "__main__":
 
@@ -28,6 +33,15 @@ if __name__ == "__main__":
     minimum = float(sys.argv[3])
     maximum = float(sys.argv[4])
     print("Range:", [minimum, maximum])
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    for video in (set(data0["video"].drop_duplicates()) & set(data1["video"].drop_duplicates())):
+        video_data0 = data0[data0["video"] == video]
+        video_data1 = data1[data1["video"] == video]
+        draw(ax, video_data0, color="royalblue", linewidth=0.5)
+        draw(ax, video_data1, color="lightcoral", linewidth=0.5)
+    fig.savefig(os.path.join(savedir, f"{os.path.basename(path0)}.{os.path.basename(path1)}.mixed.png"))
+    plt.close(fig)
 
     video_data0 = data0
     video_data1 = data1
