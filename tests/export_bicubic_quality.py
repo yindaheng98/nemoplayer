6c671160dc60e940e39dc33adc0d623c2dd28304
yindaheng98 <yindaheng98@gmail.com>
2023-04-20 03:17:40 +0000

    show pid

--- tests/export_bicubic_quality.py
+++ tests/export_bicubic_quality.py
@@ -10,6 +10,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 def task(lq_root, gt_root, video):
+    pid = os.getpid()
     length = max([int(os.path.splitext(frame)[0]) for frame in os.listdir(os.path.join(lq_root, video))])
     psnr, ssim = [0] * length, [0] * length
     for frame in os.listdir(os.path.join(lq_root, video)):
@@ -19,7 +20,7 @@ def task(lq_root, gt_root, video):
             lq = cv2.cvtColor(cv2.imread(lq_path, cv2.IMREAD_UNCHANGED), cv2.COLOR_YUV2BGR_I420)
             gt = cv2.cvtColor(cv2.imread(gt_path, cv2.IMREAD_UNCHANGED), cv2.COLOR_YUV2BGR_I420)
             hr = cv2.resize(lq, dsize=(gt.shape[1], gt.shape[0]), interpolation=cv2.INTER_CUBIC)
-            print(gt_path)
+            print(f"Process{pid:02d}", gt_path)
             frame_i = int(os.path.splitext(frame)[0])
             psnr[frame_i - 1] = peak_signal_noise_ratio(hr, gt)
             ssim[frame_i - 1] = structural_similarity(hr, gt, channel_axis=-1)
