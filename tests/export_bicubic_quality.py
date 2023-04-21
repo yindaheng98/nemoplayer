65c6340b55f666233aa4ee0280dfe82fa1f745fa
yindaheng98 <yindaheng98@gmail.com>
2023-04-21 09:52:40 +0000

    batch psnr

--- tests/export_bicubic_quality.py
+++ tests/export_bicubic_quality.py
@@ -16,31 +16,33 @@ def task(lq_root, gt_root, video):
     try:
         length = max([int(os.path.splitext(frame)[0]) for frame in os.listdir(os.path.join(lq_root, video))])
         psnr_sort, ssim_sort = [0] * length, [0] * length
+        batch_size = 16
+        for batch in range(len(os.listdir(os.path.join(lq_root, video)))//batch_size):
+            
+            gts, hrs, frame_idx = [], [], []
+            print(f"Process{pid}", "Reading", video, [batch*batch_size,(batch+1)*batch_size])
+            for frame in os.listdir(os.path.join(lq_root, video))[batch*batch_size:(batch+1)*batch_size]:
+                lq_path = os.path.join(lq_root, video, frame)
+                gt_path = os.path.join(gt_root, video, frame)
+                try:
+                    lq = cv2.cvtColor(cv2.imread(lq_path, cv2.IMREAD_UNCHANGED), cv2.COLOR_YUV2BGR_I420)
+                    gt = cv2.cvtColor(cv2.imread(gt_path, cv2.IMREAD_UNCHANGED), cv2.COLOR_YUV2BGR_I420)
+                    hr = cv2.resize(lq, dsize=(gt.shape[1], gt.shape[0]), interpolation=cv2.INTER_CUBIC)
+                    gts.append(gt)
+                    hrs.append(hr)
+                    frame_idx.append(int(os.path.splitext(frame)[0]))
+                except Exception as e:
+                    print(f"Process{pid}", e)
+            gts = torch.from_numpy(np.stack(gts)).to('cuda')
+            hrs = torch.from_numpy(np.stack(hrs)).to('cuda')
+            print(f"Process{pid}", "Calculating PSNR", video, [batch*batch_size,(batch+1)*batch_size])
+            psnr_usort = psnr(gts, hrs, data_range=255, reduction='none')
+            print(f"Process{pid}", "Calculating SSIM", video, [batch*batch_size,(batch+1)*batch_size])
+            ssim_usort = ssim(gts.permute(0,3,1,2), hrs.permute(0,3,1,2), data_range=255, reduction='none', kernel_size=7, downsample=False)
+            psnr_usort, ssim_usort = list(psnr_usort.cpu().numpy()), list(ssim_usort.cpu().numpy())
+            for i, p, s in zip(frame_idx, psnr_usort, ssim_usort):
+                psnr_sort[i-1], ssim_sort[i-1] = p, s
         
-        gts, hrs, frame_idx = [], [], []
-        print(f"Process{pid}", "Reading", video)
-        for frame in os.listdir(os.path.join(lq_root, video)):
-            lq_path = os.path.join(lq_root, video, frame)
-            gt_path = os.path.join(gt_root, video, frame)
-            try:
-                lq = cv2.cvtColor(cv2.imread(lq_path, cv2.IMREAD_UNCHANGED), cv2.COLOR_YUV2BGR_I420)
-                gt = cv2.cvtColor(cv2.imread(gt_path, cv2.IMREAD_UNCHANGED), cv2.COLOR_YUV2BGR_I420)
-                hr = cv2.resize(lq, dsize=(gt.shape[1], gt.shape[0]), interpolation=cv2.INTER_CUBIC)
-                gts.append(gt)
-                hrs.append(hr)
-                frame_idx.append(int(os.path.splitext(frame)[0]))
-            except Exception as e:
-                print(f"Process{pid}", e)
-        gts = torch.from_numpy(np.stack(gts))#.to('cuda')
-        hrs = torch.from_numpy(np.stack(hrs))#.to('cuda')
-        print(f"Process{pid}", "Calculating PSNR", video)
-        psnr_usort = psnr(gts, hrs, data_range=255, reduction='none')
-        print(f"Process{pid}", "Calculating SSIM", video)
-        ssim_usort = ssim(gts.permute(0,3,1,2), hrs.permute(0,3,1,2), data_range=255, reduction='none', kernel_size=7, downsample=False)
-        psnr_usort, ssim_usort = list(psnr_usort.cpu().numpy()), list(ssim_usort.cpu().numpy())
-        for i, p, s in zip(frame_idx, psnr_usort, ssim_usort):
-            psnr_sort[i-1], ssim_usort[i-1] = p, s
-            print(psnr_sort)
     except Exception as e:
         print(f"Process{pid}", e)
     return psnr_sort, ssim_sort
@@ -62,7 +64,7 @@ if __name__ == '__main__':
     os.makedirs(args.datadir, exist_ok=True)
 
     mp.set_start_method('spawn')
-    pool = mp.Pool(4)
+    pool = mp.Pool(1)
     videos = os.listdir(args.lq)
     results = {video: pool.apply_async(task, (args.lq, args.gt, video)) for video in videos}
 
